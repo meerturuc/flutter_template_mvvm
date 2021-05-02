1589733bba64bfd0586d9260bd0ac2e32d9799bd
Meerturuc <meerturuc@gmail.com>
2021-05-02 22:30:45 +0300

    Mobx_MVVM_Update

--- lib/view/authenticate/test/view/test_view.dart
+++ lib/view/authenticate/test/view/test_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:flutter_template/view/authenticate/test/viewmodel/test_view_model.dart';
 import '../../../../core/base/state/base_state.dart';
 import '../../../../core/base/view/base_widget.dart';
 
@@ -9,11 +10,34 @@ class TestsView extends StatefulWidget {
 }
 
 class _TestsViewState extends BaseState<TestsView> {
+  TestViewModel viewModel;
   @override
   Widget build(BuildContext context) {
-    return BaseView(
-      viewModel: "",
+    return BaseView<TestViewModel>(
+      viewModel: TestViewModel(),
+      onModelReady: (model) {
+        viewModel = model;
+      },
       onPageBuilder: (context, value) => Text("data"),
+    );
+  }
+
+  Widget get scaffoldBody => Scaffold(
+        floatingActionButton: floatingActionButtonNumber,
+        body: textNumber,
+      );
+
+  FloatingActionButton get floatingActionButtonNumber {
+    return FloatingActionButton(
+      onPressed: () => viewModel.incrementNumber(),
+    );
+  }
+
+  Widget get textNumber {
+    return Observer(
+      builder: (context) => Text(
+        viewModel.number.toString(),
+      ),
     );
   }
 }
