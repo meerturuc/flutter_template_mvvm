394f06419108b89c3f8ec65c6eabe4d01b2cda7e
Meerturuc <meerturuc@gmail.com>
2021-05-02 22:55:26 +0300

    Mobx Fix 0.2

--- lib/core/base/view/base_widget.dart
+++ lib/core/base/view/base_widget.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:mobx/mobx.dart';
 
-class BaseView<T> extends StatefulWidget {
+class BaseView<T extends Store> extends StatefulWidget {
   final Widget Function(BuildContext context, T value) onPageBuilder;
   final T viewModel;
   final Function(T model) onModelReady;
--- lib/view/authenticate/test/viewmodel/test_view_model.dart
+++ lib/view/authenticate/test/viewmodel/test_view_model.dart
@@ -4,10 +4,13 @@ part 'test_view_model.g.dart';
 class TestViewModel = _TestViewModelBase with _$TestViewModel;
 
 abstract class _TestViewModelBase with Store {
-  @observable
+  @observable // Sabit bir değer ise observable
   int number = 0;
 
-  @action
+  @computed // Bir işlem yapalıcaksa computed
+  bool get isEven => number % 2 == 0;
+
+  @action // Bir aksiyona giriyosanız action
   void incrementNumber() {
     number++;
   }
